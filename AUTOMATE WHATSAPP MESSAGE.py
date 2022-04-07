76ef551c74b95b8e67cc55e736fa8e99ce441b1f
S.YESWANTH <66507888+yeswanthsv8@users.noreply.github.com>
2022-04-07 12:01:39 +0530

    Add files via upload

---
+++ AUTOMATE WHATSAPP MESSAGE.py
@@ -0,0 +1,10 @@
+"""
+Message will be automatically sent but the Whatsweb should be loginn and laptop
+should have stable internet connections,then it will work.
+
+Module:
+pywhatkit
+"""
+
+import pywhatkit
+pywhatkit.sendwhatmsg('+919487108671','Working',12,54)

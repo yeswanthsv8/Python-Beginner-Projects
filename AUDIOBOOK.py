cf2ffc21d2da66624c0d42892bc3d0049108bc0c
S.YESWANTH <66507888+yeswanthsv8@users.noreply.github.com>
2022-04-07 11:48:20 +0530

    Add files via upload
    Text to Speech{Pdf file}
    What is the use of binary mode?
    Binary mode allows programmers to manipulate files byte by byte rather than in larger logical structures.
    
    init(): Speaker Intialisation
    runAndWait():
    This function will make the speech audible in the system, if you don't write this command then the speech will not be audible to you.
    
    Modules:
    pyttsx3: Text to Speech
    PyPDF2:  To read Pdf file

---
+++ AUDIOBOOK.py
@@ -0,0 +1,13 @@
+import pyttsx3
+import PyPDF2
+book=open('OOPS.pdf','rb')
+pdfReader=PyPDF2.PdfFileReader(book)
+pages=pdfReader.numPages
+print(pages)
+speaker=pyttsx3.init()
+
+for num in range(6,pages):
+    page=pdfReader.getPage(num)
+    text=page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
